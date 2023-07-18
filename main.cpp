fe94241ba5a9e6f30bbe4a35b6bd6c3389baa187
tobyflowers <tobiasgflowers@gmail.com>
2023-07-18 17:31:22 -0400

    :-)

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <cmath>
 #include <vector>
+#include <array>
 
 int main()
 {
@@ -167,6 +168,49 @@ public:
     return perimeter + vertices[vertices.size() -1].distance_to_point(vertices[0]);
     
   }
+};
+
+class AUV{
+public:
+std::string name;
+Point position = Point(0,0);
+double depth;
+double heading;
+std::array<double,3> speed;
+double angular_speed;
+
+AUV(std::string name,
+Point position,
+double depth,
+double heading,
+std::array<double,3> speed,
+double angular_speed){
+  this->name = name;
+  this->position = position;
+  this->depth = depth;
+  this->heading = heading;
+  this->speed = speed;
+  this->angular_speed = angular_speed;
+
+}
+
+void step(double dt)
+{
+  position.x += speed[0] * dt;  
+  position.y += speed[1] * dt;  
+  depth += speed[2] * dt;      
+}
+
+void apply_acceleration(const std::array<double, 3> acceleration, double dt) {
+  speed[0] += acceleration[0] * dt;  
+  speed[1] += acceleration[1] * dt; 
+  speed[2] += acceleration[2] * dt;  
+}
+
+void apply_angular_acceleration(double angular_acceleration, double dt) {
+    angular_speed += angular_acceleration * dt;
+  }
+
 };
 
 
